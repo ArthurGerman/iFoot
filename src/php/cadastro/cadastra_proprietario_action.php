421fd1b04ac584f2f8b3268bd6f754e8c1bbeef0
Aluno Lab 03 <aluno.lab03@ifpe.edu>
2025-11-10 08:28:17 -0300

    cadastro de proprietário

---
+++ src/php/cadastro/cadastra_proprietario_action.php
@@ -0,0 +1,23 @@
+<?php
+require_once "config.php";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $CPF = $_POST['CPF'];
+        $cidade = $_POST['cidade'];
+        $UF = $_POST['UF'];
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
+
+        $query = $pdo->prepare("INSERT INTO proprietario (nome, email, CPF, cidade, UF, endereco, telefone) VALUES (?,?,?,?,?,?,?)");
+        $query->execute([$nome, $email, $CPF, $cidade, $UF, $endereco, $telefone]);
+        
+
+        echo "Olá $nome ! Seus dados foram cadastrados com sucesso <br>";
+        echo "<button><a href='cadastra_jogador.php'>Voltar</a></button>";
+    } else{
+        echo "<strong><p style='color:red'>Requisição inválida. Não usou método POST</p></strong>";
+    }
+
+?>
